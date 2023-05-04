2290eac9d1a7acbfdb13a61b03ff74018c1cfc62
arafat717 <arafatjibon33@gmail.com>
2023-05-04 08:38:31 +0600

    fourth commit

--- src/pages/Blog/Blog.jsx
+++ src/pages/Blog/Blog.jsx
@@ -8,6 +8,9 @@ const Blog = () => {
                 <h2>1/ Tell us the differences between uncontrolled and controlled components?</h2>
                 <p>In React, controlled components refer to components that have their state and behavior controlled by the parent component. These components rely on props passed down from the parent component to update their state and behavior. Uncontrolled components refer to components that manage their own state internally.</p>
             </div>
+            <div>
+                <h2>2/ How to validate React props using PropTypes?</h2>
+            </div>
         </div>
     );
 };
